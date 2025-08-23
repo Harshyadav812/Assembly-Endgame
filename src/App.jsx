6d1959b492bd9cc31044eff022030c7688050312
Harshyadav812 <harshrao812@gmail.com>
2025-08-23 21:16:41 +0000

    Refactors App component
    Enhances game logic and styling; adds farewell message display and adjusts CSS for better layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import { useState } from 'react'
+import clsx from 'clsx'
 import './App.css'
 import Header from './components/Header'
 import GameStatus from './components/GameStatus'
 import { languages } from './language'
+import { getRandomWord, getFarewellText } from './utils'
 
 function App() {
 
-  // function getRandomWord() {
-  //   const len = Math.floor(Math.random() * 5) + 4 // 4..8
-  //   return Array.from({ length: len }, () => String.fromCharCode(97 + Math.floor(Math.random() * 26))).join('')
-  // }
-
-  const [currentWord, setCurrentWord] = useState('react')
+  const [currentWord, setCurrentWord] = useState(() => getRandomWord())
   const [guessedLetters, setGuessedLetters] = useState([])
+
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
+  const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
 
   const alphabets = "abcdefghijklmnopqrstuvwxyz"
 
   let wrongGuessCount = guessedLetters.filter(letter =>
     !currentWord.includes(letter)).length
+
 
   const isGameWon = currentWord.split('').every(letter => guessedLetters.includes(letter))
   const isGameLost = (wrongGuessCount >= languages.length - 1)
@@ -43,13 +44,29 @@ function App() {
 
     return (
       <button
+        disabled={isGameOver}
         style={styles}
         onClick={() => handleClick(letter)}
         className='key' key={idx}
-        type='button'>
+        type='button'
+        aria-disabled={guessedLetters.includes(letter)}
+        aria-label={`Letter ${letter}`}>
         {letter.toUpperCase()}
       </button>
     )
+  })
+
+  const revealWord = currentWord.split("").map((letter, idx) => {
+    const letterClassName = clsx("letter",
+      isGameLost && !guessedLetters.includes(letter) && "missed-letter",
+      isGameLost && guessedLetters.includes(letter) && "correct-letter"
+    )
+    return <span
+      key={idx}
+      className={letterClassName}>
+      {/* {!guessedLetters.includes(letter) ? letter.toUpperCase() : letter.toUpperCase()} */}
+      {letter.toUpperCase()}
+    </span>
   })
 
   const letterElements = currentWord.split("").map((letter, idx) => {
@@ -62,6 +79,7 @@ function App() {
   })
 
   const languageList = languages.map((language, idx) => {
+
     return (<span
       key={language.name}
       className={`lang-chip ${(idx < wrongGuessCount) ? 'lost' : ''}`}
@@ -74,17 +92,37 @@ function App() {
     </span>)
   })
 
-  console.log(isGameOver)
+  function displayFarewellMsg() {
+    if (!isGameOver && wrongGuessCount) {
+      const msg = getFarewellText(languages[wrongGuessCount - 1].name)
+      return <p className='farewell-msg'>{msg}</p>
+    }
+  }
+
+  function resetGame() {
+    setCurrentWord(getRandomWord())
+    setGuessedLetters([])
+  }
+
+  console.log(currentWord)
+
 
   return (
     <main>
       <Header />
-      <GameStatus isGameOver={isGameOver} isGameWon={isGameWon} isGameLost={isGameLost} />
+
+      <GameStatus
+        isLastGuessIncorrect={isLastGuessIncorrect}
+        displayFarewellMsg={displayFarewellMsg}
+        isGameOver={isGameOver}
+        isGameWon={isGameWon}
+        isGameLost={isGameLost} />
+
       <div className='language-ctn'>{languageList}</div>
-      <div className="letter-ctn">{letterElements}</div>
+      <div className="letter-ctn">{isGameLost ? revealWord : letterElements}</div>
       <div className='key-ctn'>{keys}</div>
 
-      {isGameOver && <button className="new-game">New Game</button>}
+      {isGameOver && <button onClick={resetGame} className="new-game">New Game</button>}
     </main>
   )
 }
